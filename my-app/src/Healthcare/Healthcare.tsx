cbf2c3670cbc0a44f065d33c64bffed027b9f0dc
jonhels <jonhels8888@gmail.com>
2022-08-16 16:44:02 +0200

    content adjustments and styling

--- my-app/src/Healthcare/Healthcare.tsx
+++ my-app/src/Healthcare/Healthcare.tsx
@@ -7,10 +7,12 @@ function Healthcare() {
     <div className="Healthcare">
       <div className='HealthHeader'>
         <div className='Health-content'>
-          <p className='Welcome-text'><hr className='line' />Welcome to Oval Healthcare Center. First in Care, Research and Learning.</p>
-          <h1 className='Introduction-text'>We Provide a Smart and Modern leading healthcare for people worldwide</h1>
-          <a className='Button-book' href="#">Book Appointment</a>
-          <a className='Button-find' href="#">Find a Clinic</a>
+          <p><hr className='line'></hr>Welcome to Oval Healthcare Center. First in Care, Research and Learning.</p>
+          <h1>We Provide a Smart and Modern leading healthcare for people worldwide</h1>
+          <div className='Buttons-wrapper'>
+            <a className='Button-book' href="#"><p>Book Appointment</p></a>
+            <a className='Button-find' href="#">Find a Clinic</a>
+          </div>
         </div>
         <a className='News-link' href="#"><span className='News'>NEWS</span>COVID-19 Community Testing Sites Now Open</a>
         <img className='Header-img' src={HeaderImg} alt="showcase" />
